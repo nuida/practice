ce48d78e5a440a7357fdf4074c5d4f522997f78d
nuida <nuida@mwa.biglobe.ne.jp>
2016-01-04 22:35:24 +0900

    New file

---
+++ Vernam_cipher.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<vector>
+
+// #define MAX_M_LENGTH 100
+
+std::vector<bool> enc( std::vector<bool> m, std::vector<bool> key ) {
+  using namespace std;
+  vector<bool>::iterator it_m, it_key;
+  vector<bool> res;
+  for ( it_m = m.begin(), it_key = key.begin();
+	it_m != m.end(); it_m++ ) {
+    if ( it_key == key.end() ) {
+      res.push_back( *it_m );
+    } else {
+      res.push_back( *it_m ^ *it_key );
+      it_key++;
+    }
+  }
+  return res;
+}
+
+std::vector<bool> s_to_b ( std::string str ) {
+  using namespace std;
+  vector<bool> res;
+  string::iterator it_str;
+  for ( it_str = str.begin(); it_str != str.end(); it_str++ ) {
+    res.push_back( *it_str != '0' );
+  }
+  return res;
+}
+
+int main( int argc, char* argv[] ) {
+  std::string input_m;
+  std::string::iterator it_input_m;
+  std::string input_key;
+  std::string::iterator it_input_key;
+  std::vector<bool> m;
+  std::vector<bool>::iterator it_m;
+  std::vector<bool> key;
+  std::vector<bool>::iterator it_key;
+  std::vector<bool> c;
+  std::vector<bool>::iterator it_c;
+  std::cin >> input_m;
+  m = s_to_b( input_m );
+  std::cin >> input_key;
+  key = s_to_b( input_key );
+  c = enc( m, key );
+  for ( it_c = c.begin(); it_c != c.end(); it_c++ ) {
+    std::cout << *it_c;
+  }
+  std::cout << std::endl;
+  return 0;
+}
+
