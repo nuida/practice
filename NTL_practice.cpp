0a6fc8783d58b4fd47a3df20293de7a164edcee0
nuida <nuida@mwa.biglobe.ne.jp>
2016-01-11 11:44:35 +0900

    Convertion from/to ZZ to/from string

--- NTL_practice.cpp
+++ NTL_practice.cpp
@@ -1,8 +1,35 @@
+#include <iostream>
+#include <fstream>
+#include <string>
 #include <NTL/ZZ.h>
+
+NTL::ZZ str_to_ZZ( std::string& str ) {
+  std::string::iterator it_str;
+  NTL::ZZ res;
+  for ( it_str = str.begin(); it_str != str.end(); it_str++ ) {
+    res = res * 256 + unsigned(*it_str);
+  }
+  return res;
+}
+
+std::string ZZ_to_str( NTL::ZZ zz ) {
+  std::string res;
+  while ( zz > 0 ) {
+    res.insert( res.begin(), char( zz % 256 ) );
+    zz /= 256;
+  }
+  return res;
+}
 
 int main( int argc, char* argv[] ) {
   NTL::ZZ a;
+  std::string input_str;
+  //  std::cin >> a;
+  //  std::cout << a/sizeof(unsigned char) << std::endl;
+  std::cin >> input_str;
+  std::cout << str_to_ZZ( input_str ) << std::endl;
   std::cin >> a;
-  std::cout << a*a << std::endl;
+  std::cout << ZZ_to_str( a ) << std::endl;
+  //  std::cout << unsigned(*input_str.begin()) << std::endl;
   return 0;
 }
