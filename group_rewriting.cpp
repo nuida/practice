d3fd01f79f6537c2b89d57bf0abdb895b94d67fe
Koji NUIDA <nuida@debian>
2017-01-11 22:41:19 +0900

    updated

--- group_rewriting.cpp
+++ group_rewriting.cpp
@@ -1,20 +1,79 @@
 #include<iostream>
 #include<fstream>
 #include<vector>
+#include<deque>
 #include<string>
+#include <sstream>
+
+template <typename T> std::string tostr( const T& t )
+{
+    std::ostringstream os;
+    os << t;
+    return os.str();
+}
 
 class StringGenerators{
 public:
-  StringGenerators();
-
   int id; // identify the generating set
   std::vector< std::string > gen_names; // tuple of displayed names of generators, beginning at 0-th position
   std::string error_symbol;
+
+  StringGenerators();
+
   void set_id( int ); // give the identification number
   bool add( std::string ); // add new generator (specified by name string) to the tuple
   std::string read( int ); // return the (int)-th generator's name
   int read_id(); // return the ID of the generating set
 };
+
+class ElementExpression{
+public:
+  std::deque< std::pair< int, int > > element; // sequence of generators (denoted by number) and exponents
+
+  ElementExpression(); // empty expression
+  ElementExpression( int, int ); // initialize by a power of a single generator
+
+  std::string read(); // return the expression of the element
+  void MultiplyFromLeftBy( ElementExpression ); // multiply by the input element from left
+  void MultiplyFromLeftBy( int, int ); // multiply by the input element (a power of a single generator) from left
+  void MultiplyFromRightBy( ElementExpression ); // multiply by the input element from right
+  void MultiplyFromRightBy( int, int ); // multiply by the input element (a power of a single generator) from right
+  ElementExpression operator*( const ElementExpression& ) const; // multiplication operator
+  ElementExpression InverseOf(); // return the inverse of the element
+  void ToInverse(); // convert to its inverse element
+};
+
+std::ostream& operator<<( std::ostream&, const ElementExpression& ); // output the expression of the element
+
+class ElementRelations{
+public:
+  std::vector< std::pair< ElementExpression, ElementExpression > > equality; // list of equality relations
+  std::vector< std::pair< ElementExpression, ElementExpression > > rewriting; // list of rewriting rules (from left to right)
+
+  void InsertEquality( ElementExpression, ElementExpression ); // add a new equality relation
+  void InsertRewriting( ElementExpression, ElementExpression ); // add a new rewriting relation
+
+/*
+  ElementExpression(); // empty expression
+  ElementExpression( int, int ); // initialize by a power of a single generator
+
+  std::deque< std::pair< int, int > > element; // sequence of generators (denoted by number) and exponents
+
+  std::string read(); // return the expression of the element
+  void MultiplyFromLeftBy( ElementExpression ); // multiply by the input element from left
+  void MultiplyFromLeftBy( int, int ); // multiply by the input element (a power of a single generator) from left
+  void MultiplyFromRightBy( ElementExpression ); // multiply by the input element from right
+  void MultiplyFromRightBy( int, int ); // multiply by the input element (a power of a single generator) from right
+  ElementExpression operator*( const ElementExpression& ) const; // multiplication operator
+  ElementExpression InverseOf(); // return the inverse of the element
+  void ToInverse(); // convert to its inverse element
+*/
+};
+
+
+//
+// definitions for StringGenerators
+//
 
 StringGenerators::StringGenerators() {
   this->error_symbol = std::string("error");
@@ -43,11 +102,154 @@ int StringGenerators::read_id() {
   return this->id;
 }
 
+//
+// definitions for ElementExpression
+//
+
+ElementExpression::ElementExpression() {}
+
+ElementExpression::ElementExpression( int idx, int e ) {
+  this->element.push_back( std::pair< int, int >( idx, e ) );
+}
+
+std::string ElementExpression::read() {
+  std::string res;
+  std::deque< std::pair< int, int > >::iterator it;
+  for ( it = this->element.begin(); it != this->element.end(); ++it ) {
+    if ( it != this->element.begin() ) {
+      res += std::string(" ");
+    }
+    res += std::string( tostr( it->first ) )
+      + std::string("^")
+      + std::string( tostr( it->second ) );
+  }
+  return res;
+}
+
+std::ostream& operator<<( std::ostream& os, const ElementExpression& elm ) {
+  std::deque< std::pair< int, int > >::const_iterator it;
+  for ( it = elm.element.begin(); it != elm.element.end(); ++it ) {
+    if ( it != elm.element.begin() ) {
+      os << std::string(" ");
+    }
+    os << std::string( tostr( it->first ) )
+      + std::string("^")
+      + std::string( tostr( it->second ) );
+  }
+  return os;
+}
+
+void ElementExpression::MultiplyFromLeftBy( ElementExpression elm ) {
+  bool b = true;
+
+  while ( b ) {
+    if ( this->element.empty() || elm.element.empty() || this->element.front().first != elm.element.back().first ) {
+      b = false;
+    } else {
+      this->element.front().second += elm.element.back().second;
+      elm.element.pop_back();
+      if ( this->element.front().second == 0 ) {
+	this->element.pop_front();
+      }
+    }
+  }
+  while ( !elm.element.empty() ) {
+    this->element.push_front( elm.element.back() );
+    elm.element.pop_back();
+  }
+}
+
+void ElementExpression::MultiplyFromLeftBy( int idx, int e ) {
+  this->MultiplyFromLeftBy( ElementExpression( idx, e ) );
+}
+
+void ElementExpression::MultiplyFromRightBy( ElementExpression elm ) {
+  bool b = true;
+
+  while ( b ) {
+    if ( this->element.empty() || elm.element.empty() || this->element.back().first != elm.element.front().first ) {
+      b = false;
+    } else {
+      this->element.back().second += elm.element.front().second;
+      elm.element.pop_front();
+      if ( this->element.back().second == 0 ) {
+	this->element.pop_back();
+      }
+    }
+  }
+  while ( !elm.element.empty() ) {
+    this->element.push_back( elm.element.front() );
+    elm.element.pop_front();
+  }
+}
+
+void ElementExpression::MultiplyFromRightBy( int idx, int e ) {
+  this->MultiplyFromRightBy( ElementExpression( idx, e ) );
+}
+
+ElementExpression ElementExpression::operator*( const ElementExpression& elm ) const {
+  ElementExpression res(*this);
+  res.MultiplyFromRightBy( elm );
+  return res;
+}
+
+ElementExpression ElementExpression::InverseOf() {
+  ElementExpression res;
+  std::deque< std::pair< int, int > >::iterator it;
+  for ( it = this->element.begin(); it != this->element.end(); ++it ) {
+    res.MultiplyFromLeftBy( it->first, -it->second );
+  }
+  return res;
+}
+
+void ElementExpression::ToInverse() {
+  (*this) = this->InverseOf();
+}
+
+//
+// definitions for ElementRelations
+//
+
+void ElementRelations::InsertEquality( ElementExpression ex1, ElementExpression ex2 ){
+
+}
+
+/*
+class ElementRelations
+public:
+  std::vector< std::pair< ElementExpression, ElementExpression > > equality; // list of equality relations
+  std::vector< std::pair< ElementExpression, ElementExpression > > rewriting; // list of rewriting rules (from left to right)
+
+  void InsertEquality( ElementExpression, ElementExpression ); // add a new equality relation
+  void InsertRewriting( ElementExpression, ElementExpression ); // add a new rewriting relation
+*/
+
+//
+// main
+//
 
 int main( int argc, char* argv[] ) {
   StringGenerators gen;
 
+  ElementExpression elm( 1, 3 );
+  ElementExpression elm2( 2, -3 );
+  ElementExpression elm3;
+
+  elm.MultiplyFromLeftBy( 2, -2 );
+  elm.MultiplyFromRightBy( ElementExpression( 2, 3 ) );
+  elm2.MultiplyFromRightBy( ElementExpression( 1, -3 ) );
+  elm.MultiplyFromRightBy( elm2 );
+
   std::cout << gen.read(3) << std::endl;
+  std::cout << elm3 << std::endl;
+  std::cout << elm << std::endl;
+  std::cout << elm2 << std::endl;
+  elm3 = elm * elm2;
+  std::cout << elm3 << std::endl;  
+  elm3 = elm3.InverseOf();
+  std::cout << elm3 << std::endl;  
+  elm3.ToInverse();
+  std::cout << elm3 << std::endl;  
 
   return 0;
 }
